32062b0e6b1a79bc6c39befc863c0d59d5bb2369
Kim Carter <Kim.Carter@binarymist.net>
2021-12-21 11:40:32 +1300

    Change license from agpl-3.0 to bsl

--- src/steps/steps.js
+++ src/steps/steps.js
@@ -1,3 +1,12 @@
+// Copyright (C) 2017-2022 BinaryMist Limited. All rights reserved.
+
+// Use of this software is governed by the Business Source License
+// included in the file /licenses/bsl.md
+
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 // features/support/steps.js
 const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
